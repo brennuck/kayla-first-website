71b232cb994c51de2cdb3a695b78379e50f579df
Brennon Nuckols <brennuck@gmail.com>
2020-12-29 14:55:47 -0700

    daf

---
+++ src/kayla-face.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Nood from './nood.jpg'
+import './App.css';
+
+function KaylaFace() {
+    return <img src={Nood} alt="amazing girl" className="picture" />
+}
+
+export default KaylaFace
